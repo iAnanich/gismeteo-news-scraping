a8ed2cf5c34208fb4f445aae29707355948fe08d
Illia Ananich <illia.ananich@gmail.com>
2017-06-04 19:15:49 +0300

    edit `setup.py` - add `client-secret.json` as data file, remove project ID from `scrapinghub.yml`

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     name         = 'project',
     version      = '0.1',
     packages     = find_packages(),
+    data_files   = [('sc200327', ['sc200327/client-secret.json'])],
     entry_points = {'scrapy': ['settings = sc200327.settings']},
 )
