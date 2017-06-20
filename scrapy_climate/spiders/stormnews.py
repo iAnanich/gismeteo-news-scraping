091b1acbdb102ca4c454b5ad3b981104e135afb0
Illia Ananich <illia.ananich@gmail.com>
2017-06-20 15:08:27 +0300

    add `stormnews` spider

---
+++ scrapy_climate/spiders/stormnews.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from ..spider import TemplateSpider
+
+
+class StormNewsSpider(TemplateSpider):
+    name = 'stormnews'
+
+    _start_path = ''
+    _start_domain = 'stormnews.ru'
+    _protocol = 'http'
+
+    _css_selector_article = 'article[class="boxes"]'
+    _xpath_selector_list_tags = ['strong/a/text()', ]
+    _xpath_selector_list_text = ['div/p/text()', ]
+    _xpath_selector_list_header = ['header/h1/text()', ]
+    _css_selector_news_list = 'article[class="articlebox articleboxsmall"]'
+    _xpath_selector_path = 'div/header/h2/a/@href'
+
+    def parse(self, response):
+        self._scraped_indexes = self._scraped_in_past
+        # extract url from main article in img,
+        spotted_event = response.css('article[class="articlebox"]')[0]
+        path = spotted_event.xpath('div/header/h1/a/@href').extract_first()
+        yield from self._yield_request(path)
+        # extract urls from list
+        yield from self._yield_requests_from_response(response)
+
+    def _convert_path_to_index(self, path: str) -> str:
+        return path.split('/')[-1]
